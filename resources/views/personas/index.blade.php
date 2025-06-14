f5a97ff16cc6eea21ba5029d19c611293453c058
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2025-06-13 23:54:35 -0500

    Cambiar la ruta principal para que redirija a la vista de 'personas.index' en lugar de 'welcome'.

---
+++ resources/views/personas/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es"></html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Personas</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Personas
+                            <a href="" class="btn btn-success btn-sm float-end">Crear Persona</a>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/personas', function () {
+    return view('personas.index');
 });
 
 Route::get('/dashboard', function () {
