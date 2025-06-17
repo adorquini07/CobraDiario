fa71575495771922fbc171c74bc8ab68a7330a63
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2025-06-16 20:43:02 -0500

    Agregar Livewire como dependencia en composer.json y actualizar las vistas para incluir los estilos y scripts de Livewire. Se renombró la sección en 'index.blade.php' para 'contenidossss' y se ajustó la estructura HTML en 'main.blade.php'.

--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -1,46 +1,45 @@
 @extends('layouts.main')
-@section('contenido')
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            Personas
-                            <a href="{{ route('personas.create') }}" class="btn btn-success btn-sm float-end">Crear Persona</a>
-                        </div>
-                        <div class="card-body">
-                            @if (session('info'))
-                                <div class="alert alert-success">
-                                    {{ session('info') }}
-                                </div>
-                            @endif
-                            <table class="table table-bordered table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>Cédula</th>
-                                        <th>Celular</th>
-                                        <th>Dirección</th>
-                                        <th>Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($personas as $persona)
-                                        <tr>
-                                            <td>{{ $persona->nombre }}</td>
-                                            <td>{{ $persona->apellido }}</td>
-                                            <td>{{ $persona->nuip }}</td>
-                                            <td>{{ $persona->telefono }}</td>
-                                            <td>{{ $persona->direccion }}</td>
-                                            
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+@section('contenidossss')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Personas
+                    <a href="{{ route('personas.create') }}" class="btn btn-success btn-sm float-end">Crear Persona</a>
+                </div>
+                <div class="card-body">
+                    @if (session('info'))
+                    <div class="alert alert-success">
+                        {{ session('info') }}
                     </div>
+                    @endif
+                    <table class="table table-bordered table-sm">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Cédula</th>
+                                <th>Celular</th>
+                                <th>Dirección</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($personas as $persona)
+                            <tr>
+                                <td>{{ $persona->nombre }}</td>
+                                <td>{{ $persona->apellido }}</td>
+                                <td>{{ $persona->nuip }}</td>
+                                <td>{{ $persona->telefono }}</td>
+                                <td>{{ $persona->direccion }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
